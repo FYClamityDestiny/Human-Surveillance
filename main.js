fcd078db52e28e450ed1d222692853bff54f1839
FYClamityDestiny <99613329+FYClamityDestiny@users.noreply.github.com>
2023-06-25 09:11:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,35 +19,37 @@ function modelLoaded() {
     statuse = true;
 }
 function draw() {
-    objectdetector.detect(video, gotResult);
-    r = random(255);
-    g = random(255);
-    b = random(255);
     image(video, 0, 0, 380, 380);
     if (statuse != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectdetector.detect(video,gotResult);
         for (i = 0; i < objects.length; i++) {
-            if (objects == "person") {
-                music.stop();
-                document.getElementById("status").innerHTML = "Status:Baby Detected";
-                document.getElementById("number_of_objects").innerHTML = objects.length + "objects were detected";
+            document.getElementById("status").innerHTML = "Status:Baby Detected";
                 fill(r, g, b);
                 percent = floor(objects[i].confidence * 100);
                 text(objects[i].label + ":" + percent + "%", objects[i].x + 10, objects[i].y + 10);
                 noFill();
                 stroke(r, g, b);
                 rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if (objects[i].label == "person") {
+                music.stop();
+                document.getElementById("number_of_objects").innerHTML =  "Baby is detected";
+              
             }
             else{
                 document.getElementById("status").innerHTML = "Status:Baby not Detected";
                 music.play();
             }
-            if(objects.length < 0){
-                document.getElementById("status").innerHTML = "Status:Baby not Detected";
+        }
+            if(objects.length == 0){
+                document.getElementById("number_of_objects").innerHTML = "Baby not Detected";
                 music.play();
             }
         }
     }
-}
+
 
 function gotResult(error, results) {
     if (error) {
